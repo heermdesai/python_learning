244c7ed2306cca41e838caf33575bbc164946e3f
heermdesai <heermdesai@gmail.com>
2025-08-12 17:20:05 -0400

    adding numGuesser game

---
+++ numGuesser.py
@@ -0,0 +1,24 @@
+import random
+
+high = 10
+low = 1
+numChance = 10
+numGuesses = 0
+
+print("I am thinking of a number between 1 and 10. Try to guess what it is!")
+
+num = random.randrange(low, high + 1)
+print(num)
+
+while(numGuesses < numChance):
+    guess = int(input("Enter your guess: "))
+    if (guess == num):
+       numGuesses += 1
+       print("Correct! You win!")
+       break
+    else:
+      numGuesses += 1
+      print("Wrong! Try again!")
+
+if (numGuesses == numChance):
+   print("Sorry, you're out of guesses. The number was " + num)
